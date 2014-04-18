598f29fcfd133318e75ee249f9e73aa29c335b1d
ladishala <lavdishala@hotmail.com>
2014-04-18 02:01:57 +0200

    First Commit

---
+++ Adresat/Sherbimi.asmx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Net.Http;
+using System.Xml;
+
+namespace Adresat
+{
+    /// <summary>
+    /// Summary description for Sherbimi
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Sherbimi : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string ktheAdresen(string lat, string lng)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("http://nominatim.openstreetmap.org/reverse?format=xml&lat=" + lat + "&lon=" + lng + "&addressdetails=1");
+            XmlNode nod1 = doc.SelectSingleNode("/reversegeocode/result");
+            return nod1.InnerText;
+        }
+    }
+}
---
+++ Adresat/obj/Release/Package/PackageTmp/Sherbimi.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Sherbimi.asmx.cs" Class="Adresat.Sherbimi" %>
